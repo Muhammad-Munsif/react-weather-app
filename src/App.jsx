527f73eed213c6f759f7aac34ffcade12536949d
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-04 15:19:03 +0500

    To create a weather app in react js

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Weather from './components/Weather'
 const App = () => {
   return (
-    <div className='bg-gray-200 min-h-screen py-12'>
+    <div className='bg-gray-200 min-h-screen pt-12 flex items-center flex-col'>
       <h1 className='font-bold text-3xl text-center'>Weather App in React</h1>
       <Weather />
     </div>
